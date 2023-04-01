3938404d2d5bb67a35bcc886ec9dcf2fe81b9395
Emanuel Carrero <89891608+emanuelmcp@users.noreply.github.com>
2023-04-01 19:07:04 +0200

    chore: Backup

--- pokemon-game/src/components/PokemonOptions.vue
+++ pokemon-game/src/components/PokemonOptions.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="options-container">
     <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id">{{ pokemon.name }}</li>
+      <li
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        @click="$emit('selectionPokemon')"
+      >
+        {{ pokemon.name }}
+      </li>
     </ul>
   </div>
 </template>
@@ -13,6 +19,9 @@ export default {
       type: Array,
       required: true,
     },
+  },
+  methods: {
+    hola() {},
   },
 };
 </script>
